8071b566bd34b8204547cdb5dbef391d8dfd909f
Rodrigodeveloperjr <rodrigojsdeveloper@gmail.com>
2023-03-16 18:28:39 -0300

    style: global style created and configured

---
+++ src/styles/global.ts
@@ -0,0 +1,57 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+  * {
+    border: 0;
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+    scroll-behavior: smooth;
+    font-family: 'Inter' sans-serif;
+  }
+
+  :root {
+    // Primary Palette
+    --color-primary: #FF2253;
+    --color-primary-50: #FFE9EE;
+    --color-secondary: #4ACA94;
+    --color-secondary-20: #39DF8F;
+    --color-secondary-50: #E4F7EF;
+    
+    // Product Palette
+    --product-1: #EEF6FF;
+    --product-2: #FFF7F4;
+    --product-3: #FFE4EA;
+    --product-4: #F3EEF5;
+  
+    // Grey Scale Palette
+    --grey-1: #F8F9FA;
+    --grey-2: #E9ECEF;
+    --grey-3: #868E96;
+    --grey-4: #212529;
+    --grey-5: #DEE2E6;
+
+    // Colors default
+    --color-white: #FFFFFF;
+    --color-black: #000000;
+
+    // Shadow
+    --shadow-1: rgba(0, 0, 0, 0.5);
+    --shadow-2: rgba(0, 0, 0, 0.15);
+    --shadow-3: rgba(0, 0, 0, 0.25);
+  }
+
+  body {
+    background-color: var(--color-white);
+    color: var(--color-black);
+  }
+
+  button, a {
+    cursor: pointer;
+  }
+
+  a {
+    text-decoration: none;
+  }
+`;
