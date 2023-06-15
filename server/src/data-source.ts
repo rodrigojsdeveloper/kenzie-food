79b0a6cf53fc86f669ec1358d5661730e820fc2e
Rodrigodeveloperjr <rodrigojsdeveloper@gmail.com>
2023-06-15 18:03:51 -0300

    feat: create user entity

--- server/src/data-source.ts
+++ server/src/data-source.ts
@@ -5,8 +5,8 @@ import "dotenv/config";
 
 const AppDataSource = new DataSource({
   type: "postgres",
-  host: "localhost",
-  port: 5432,
+  host: process.env.POSTGRES_HOST,
+  port: Number(process.env.POSTGRES_PORT),
   username: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DATABASE,
---
+++ server/src/entities/user.entity.ts
@@ -0,0 +1,26 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+} from "typeorm";
+
+@Entity("users")
+class User {
+  @PrimaryGeneratedColumn("uuid")
+  readonly id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+}
+
+export { User };
