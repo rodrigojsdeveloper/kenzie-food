4cfebe66b5d7a369ef33952b659d4aa639f86b7a
Rodrigodeveloperjr <rodrigojsdeveloper@gmail.com>
2023-06-16 13:19:07 -0300

    feat: create schema validation middleware

--- server/src/routes/user.routes.ts
+++ server/src/routes/user.routes.ts
@@ -2,10 +2,18 @@ import { Router } from "express";
 
 import { UsersController } from "../controllers/user.controller";
 
+import { schemaValidationMiddleware } from "../middlewares/schemaValidation.middleware";
+
+import { userSchema } from "../schemas/user.schema";
+
 const routes = Router();
 
 const userRoutes = (): Router => {
-  routes.post("/signup", new UsersController().create);
+  routes.post(
+    "/signup",
+    schemaValidationMiddleware(userSchema),
+    new UsersController().create
+  );
 
   return routes;
 };
