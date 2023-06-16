11aabb84b800a85de010f47dbdcd5fc1f99dd10c
Rodrigodeveloperjr <rodrigojsdeveloper@gmail.com>
2023-06-16 16:32:56 -0300

    feat: create login route

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,9 +1,11 @@
 import { Express } from "express";
 
+import { loginRoutes } from "./login.routes";
 import { userRoutes } from "./user.routes";
 
 const appRoutes = (app: Express): void => {
   app.use("/users", userRoutes());
+  app.use("/signin", loginRoutes());
 };
 
 export { appRoutes };
---
+++ server/src/routes/login.routes.ts
@@ -0,0 +1,13 @@
+import { Router } from "express";
+
+import { LoginController } from "../controllers/login.controller";
+
+const routes = Router();
+
+const loginRoutes = (): Router => {
+  routes.post("", new LoginController().login);
+
+  return routes;
+};
+
+export { loginRoutes };
